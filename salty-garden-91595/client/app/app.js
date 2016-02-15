760b5675f4869a22baa385d5e7479c7b9209b9c8
Terrell Quillin <terrellq@gmail.com>
2016-02-15 11:03:12 -0700

    updates repo and tries again to push to heroku.

--- salty-garden-91595/client/app/app.js
+++
@@ -1,5 +0,0 @@
-console.log('on est le client l\'ElectricLadyland...');
-
-
-// the second (sometimes first) step to introducing angular into your web application
-var app = angular.module('ElectricLadyland', ['mainController', 'productsApiFactory']);
--- salty-garden-91595/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res){
-  res.render( 'index', {title: "Here are products"});
-
-});
-
-module.exports = router;
